771a906e0661947d9de533f9a62df1bfc3dbb0ba
aixmar <aixamarianag@gmail.com>
2023-03-15 16:44:00 -0300

    feat(Back/CreateIngredients): add pizza ingredient

--- Api/src/update/createIngredients.js
+++ Api/src/update/createIngredients.js
@@ -1,22 +1,46 @@
 const { Ingredient } = require('../db');
 
 const ingredients = [
-    { name: 'aceitunas', image: 'https://ejemplo.com/aceitunas.jpg', price: 199 },
-    { name: 'jamon', image: 'https://ejemplo.com/jamon.jpg', price: 499 },
-    { name: 'pollo', image: 'https://ejemplo.com/pollo.jpg', price: 699 },
-    { name: 'lomo', image: 'https://ejemplo.com/lomo.jpg', price: 799 },
-    { name: 'cebolla', image: 'https://ejemplo.com/cebolla.jpg', price: 149 },
-    { name: 'champiniones', image: 'https://ejemplo.com/champiniones.jpg', price: 349 },
-    { name: 'tomates', image: 'https://ejemplo.com/tomates.jpg', price: 299 },
-    { name: 'albahaca', image: 'https://ejemplo.com/albahaca.jpg', price: 99 },
-    { name: 'ajo', image: 'https://ejemplo.com/ajo.jpg', price: 49 },
-    { name: 'espinaca', image: 'https://ejemplo.com/espinaca.jpg', price: 199 },
-    { name: 'roquefort', image: 'https://ejemplo.com/roquefort.jpg', price: 599 },
-    { name: 'anchoas', image: 'https://ejemplo.com/anchoas.jpg', price: 399 },
-    { name: 'atún', image: 'https://ejemplo.com/atun.jpg', price: 249 },
-    { name: 'piña', image: 'https://ejemplo.com/pina.jpg', price: 199 },
-    { name: 'mortadela', image: 'https://ejemplo.com/mortadela.jpg', price: 299 },
-    { name: 'oregano', image: 'https://ejemplo.com/oregano.jpg', price: 49 }
+ 
+ 
+    { name: 'Lomo', image: 'https://ejemplo.com/lomo.jpg', category: "proteins", veggie: false, price: 799 },
+    { name: 'Tomatoes', image: 'https://ejemplo.com/tomates.jpg', category: "fruit", veggie: true, price: 299 },
+    { name: 'Albahaca', image: 'https://ejemplo.com/albahaca.jpg', category: "toppings", veggie: true, price: 99 },
+    { name: 'Garlic', image: 'https://ejemplo.com/ajo.jpg', category: "toppings", veggie: true, price: 49 },
+    { name: 'Spinach', image: 'https://ejemplo.com/espinaca.jpg',category: "vegetable", veggie: true, price: 199 },
+    { name: 'Blue cheese', image: 'https://ejemplo.com/roquefort.jpg',category: "cheese", veggie: true, price: 599 },
+    { name: 'Tuna', image: 'https://ejemplo.com/atun.jpg', category: "proteins", veggie: false, price: 249 },
+    { name: 'Pinnaple', image: 'https://ejemplo.com/pina.jpg', category: "fruit", veggie: true, price: 199 },
+    { name: 'Mortadella', image: 'https://ejemplo.com/mortadela.jpg', category: "proteins", veggie: false, price: 299 },
+    { name: 'Oregano', image: 'https://ejemplo.com/oregano.jpg',category: "toppings", veggie: true, price: 49 },
+      { name: 'Parmesan', image: 'https://ejemplo.com/cheese.jpg', category: 'cheese', veggie: true, price: 69 },
+      { name: 'Provolone', image: 'https://ejemplo.com/cheese.jpg', category: 'cheese', veggie: true, price: 69 },
+      { name: 'Cheddar', image: 'https://ejemplo.com/cheese.jpg', category: 'cheese', veggie: true, price: 69 },
+      { name: 'Mozzarella', image: 'https://ejemplo.com/cheese.jpg', category: 'cheese base', veggie: true, price: 69 },
+      { name: 'Lactose-Free Mozzarella', image: 'https://ejemplo.com/cheese.jpg', category: 'cheese base', veggie: true, price: 69 },
+      { name: 'Vegan Mozzarella', image: 'https://ejemplo.com/cheese.jpg', category: 'cheese base', veggie: true, price: 69 },
+      { name: 'Tomato sauce', image: 'https://ejemplo.com/tomato-sauce.jpg', category: 'Sauce', veggie: true, price: 39 },
+      { name: 'Without Mozzarella', image: 'https://ejemplo.com/cheese.jpg', category: 'cheese base', veggie: true, price: 00 },
+      { name: 'Gluten-Free dough', image: 'https://ejemplo.com/cheese.jpg', category: 'dough', veggie: true, price: 120 },
+      { name: 'Wholemeal flour dough', image: 'https://ejemplo.com/cheese.jpg', category: 'dough', veggie: true, price: 120 },
+      { name: 'Wheat flour dough', image: 'https://ejemplo.com/cheese.jpg', category: 'dough', veggie: true, price: 120 },
+      { name: 'Pepperoni', image: 'https://ejemplo.com/pepperoni.jpg', category: 'Meat', veggie: false, price: 79 },
+      { name: 'Sausage', image: 'https://ejemplo.com/sausage.jpg', category: 'Meat', veggie: false, price: 89 },
+      { name: 'Bacon', image: 'https://ejemplo.com/bacon.jpg', category: 'Meat', veggie: false, price: 99 },
+      { name: 'Ham', image: 'https://ejemplo.com/ham.jpg', category: 'Meat', veggie: false, price: 89 },
+      { name: 'Chicken', image: 'https://ejemplo.com/chicken.jpg', category: 'Meat', veggie: false, price: 89 },
+      { name: 'Beef', image: 'https://ejemplo.com/beef.jpg', category: 'Meat', veggie: false, price: 99 },
+      { name: 'Meatballs', image: 'https://ejemplo.com/meatballs.jpg', category: 'Meat', veggie: false, price: 99 },
+      { name: 'Anchovies', image: 'https://ejemplo.com/anchovies.jpg', category: 'Fish', veggie: false, price: 109 },
+      { name: 'Mushrooms', image: 'https://ejemplo.com/mushrooms.jpg', category: 'Toppings', veggie: true, price: 59 },
+      { name: 'Onions', image: 'https://ejemplo.com/onions.jpg', category: 'Toppings', veggie: true, price: 49 },
+      { name: 'Bell peppers', image: 'https://ejemplo.com/bell-peppers.jpg', category: 'Toppings', veggie: true, price: 59 },
+      { name: 'Jalapeños', image: 'https://ejemplo.com/jalapenos.jpg', category: 'Toppings', veggie: true, price: 69 },
+      { name: 'Pineapple', image: 'https://ejemplo.com/pineapple.jpg', category: 'Toppings', veggie: true, price: 69 },
+      { name: 'Olives', image: 'https://ejemplo.com/olives.jpg', category: 'Toppings', veggie: true, price: 49 },
+      { name: 'Tomatoes', image: 'https://ejemplo.com/tomatoes.jpg', category: 'Toppings', veggie: true, price: 59 },
+     
+    
   ];
   
 const createIngredients = async () => {
