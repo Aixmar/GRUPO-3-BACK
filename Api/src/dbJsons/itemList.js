94ffd52628333c13cb915d4949be314c400f2aec
MuraJoaquin <joaco_mura@hotmail.com>
2023-03-19 03:59:41 -0300

    add(itemList): add sides(postres,extras)

--- Api/src/dbJsons/itemList.js
+++ Api/src/dbJsons/itemList.js
@@ -651,72 +651,249 @@ const itemList = [
       ],
       meatIngredients: ["Meatballs", "Pepperoni", "Bacon"],
     },
-    category:"pizza",
+    category: "pizza",
   },
   {
-    name:"Coca-Cola",
-    image:"https://i.postimg.cc/Z5y4bZQh/cocacola.png",
-    price:1.22,
-    detail:{},
+    name: "Coca-Cola",
+    image: "https://i.postimg.cc/Z5y4bZQh/cocacola.png",
+    price: 1.22,
+    detail: {},
     category: "drinks",
- },
- {
-    name:"Diet Coke",
-    image:"https://i.postimg.cc/V6z8WtkY/dietcoke.png",
-    price:1.40,
-    detail:{},
+  },
+  {
+    name: "Diet Coke",
+    image: "https://i.postimg.cc/V6z8WtkY/dietcoke.png",
+    price: 1.40,
+    detail: {},
     category: "drinks",
- },
- {
-    name:"Fanta",
-    image:"https://i.postimg.cc/CKTYdXyn/fanta.png",
-    price:1.22,
-    detail:{},
+  },
+  {
+    name: "Fanta",
+    image: "https://i.postimg.cc/CKTYdXyn/fanta.png",
+    price: 1.22,
+    detail: {},
     category: "drinks",
- },
- {
-    name:"Sprite",
-    image:"https://i.postimg.cc/cHTN9Jvf/sprite.png",
-    price:1.22,
-    detail:{},
+  },
+  {
+    name: "Sprite",
+    image: "https://i.postimg.cc/cHTN9Jvf/sprite.png",
+    price: 1.22,
+    detail: {},
     category: "drinks",
- },
- {
-    name:"Water",
-    image:"https://i.postimg.cc/rFHq2zQJ/agua.png",
-    price:1.15,
-    detail:{},
+  },
+  {
+    name: "Water",
+    image: "https://i.postimg.cc/rFHq2zQJ/agua.png",
+    price: 1.15,
+    detail: {},
     category: "drinks",
- },
- {
-    name:"Strawberry Fanta",
-    image:"https://i.postimg.cc/yxdCwzRt/redfanta.png",
-    price:1.22,
-    detail:{},
+  },
+  {
+    name: "Strawberry Fanta",
+    image: "https://i.postimg.cc/yxdCwzRt/redfanta.png",
+    price: 1.22,
+    detail: {},
     category: "drinks",
- },
- {
-    name:"RockStar Orange",
-    image:"https://i.postimg.cc/436GcPZL/rockstarorange.png",
-    price:1.50,
-    detail:{},
+  },
+  {
+    name: "RockStar Orange",
+    image: "https://i.postimg.cc/436GcPZL/rockstarorange.png",
+    price: 1.50,
+    detail: {},
     category: "drinks",
- },
- {
-    name:"Splash Lemonade",
-    image:"https://i.postimg.cc/SsMh6Y3s/lemonade.png",
-    price:1.33,
-    detail:{},
+  },
+  {
+    name: "Splash Lemonade",
+    image: "https://i.postimg.cc/SsMh6Y3s/lemonade.png",
+    price: 1.33,
+    detail: {},
     category: "drinks",
- },
+  },
 
- {
-    name:"Vitamin Water",
-    image:"https://i.postimg.cc/FzPXmbJT/gatorade.png",
-    price:2.50,
-    detail:{},
+  {
+    name: "Vitamin Water",
+    image: "https://i.postimg.cc/FzPXmbJT/gatorade.png",
+    price: 2.50,
+    detail: {},
     category: "drinks",
- },
+  },
+  {
+    name: "Cookie dough brownie made with M&M’S minis chocolate candies",
+    image: "https://i.postimg.cc/vmthsH5K/brownie2.jpg",
+    price: 3.99,
+    description: {},
+    detail: {
+      calories: 300,
+      fat: 15,
+      carbs: 40,
+      protein: 5
+    },
+    category: "sides"
+  },
+  {
+    name: "Cookie dough brownie made with TWIX cookie bar pieces",
+    image: "https://i.postimg.cc/fRf0CM1W/brownie3.jpg",
+    price: 3.99,
+    description: {},
+    detail: {
+      calories: 250,
+      fat: 10,
+      carbs: 35,
+      protein: 3
+    },
+    category: "sides"
+  },
+
+  // Ejemplo 1: Cheesecake de fresa
+  // {
+  //   name: "Cheesecake de fresa",
+  //   image: "ruta/de/la/imagen",
+  //   price: 4.50,
+  //   description: {},
+  //   detail: {
+  //     calories: 400,
+  //     fat: 25,
+  //     carbs: 45,
+  //     protein: 8
+  //   },
+  //   category: "sides"
+  // },
+
+  // Ejemplo 2: Tarta de manzana
+  // {
+  //   name: "Tarta de manzana",
+  //   image: "ruta/de/la/imagen",
+  //   price: 3.75,
+  //   description: {},
+  //   detail: {
+  //     calories: 350,
+  //     fat: 20,
+  //     carbs: 40,
+  //     protein: 6
+  //   },
+  //   category: "sides"
+  // },
+
+  // Ejemplo 3: Flan de vainilla
+  // {
+  //   name: "Flan de vainilla",
+  //   image: "ruta/de/la/imagen",
+  //   price: 3.00,
+  //   description: {},
+  //   detail: {
+  //     calories: 200,
+  //     fat: 8,
+  //     carbs: 30,
+  //     protein: 4
+  //   },
+  //   category: "sides"
+  // },
+  {
+    name: "Mix2Pizza WINGS – Oven roasted",
+    image: "https://i.postimg.cc/CKS0Sgv2/wings-oven-roasted.jpg",
+    price: 7.99,
+    description: {},
+    detail: {
+      calories: 510,
+      fat: 35,
+      carbs: 3,
+      protein: 47
+    },
+    category: "sides"
+  },
+  {
+    name: "Mix2Pizza WINGS – BBQ",
+    image: "https://i.postimg.cc/G2pH3YM6/WINGS-BBQ.jpg",
+    price: 7.99,
+    description: {},
+    detail: {
+      calories: 630,
+      fat: 35,
+      carbs: 32,
+      protein: 48
+    },
+    category: "sides"
+  },
+  {
+    name: "Mix2Pizza WINGS – Garlic parmesan",
+    image: "https://i.postimg.cc/3xtMKydD/wings-parmesan-2.jpg",
+    price: 7.99,
+    description: {},
+    detail: {
+      calories: 660,
+      fat: 49,
+      carbs: 7,
+      protein: 49
+    },
+    category: "sides"
+  },
+  {
+    name: "Mix2Pizza DIPS - Crazy sauce",
+    image: "https://i.postimg.cc/zB0jcnMB/crazy-sauce.jpg",
+    price: 0.99,
+    description: {},
+    detail: {
+      calories: 30,
+      fat: 0,
+      carbs: 7,
+      protein: 1
+    },
+    category: "sides"
+  },
+  {
+    name: "Mix2Pizza DIPS - Butter garlic flavor",
+    image: "https://i.postimg.cc/t4nV0h1T/caesar-dip-sauce.jpg",
+    price: 0.75,
+    description: {},
+    detail: {
+      calories: 370,
+      fat: 42,
+      carbs: 0,
+      protein: 0
+    },
+    category: "sides"
+  },
+  {
+    name: "Mix2Pizza DIPS - Buffalo ranch",
+    image: "https://i.postimg.cc/43JZvGTg/caesar-dip-buffalo.jpg",
+    price: 0.75,
+    description: {},
+    detail: {
+      calories: 230,
+      fat: 24,
+      carbs: 3,
+      protein: 2
+    },
+    category: "sides"
+  },
+  {
+    name: "Mix2Pizza DIPS - Cheezy jalapeño",
+    image: "https://i.postimg.cc/9MDfT2Gf/caesar-dip-jalapeno.jpg",
+    price: 0.75,
+    description: {},
+    detail: {
+      calories: 220,
+      fat: 22,
+      carbs: 3,
+      protein: 1
+    },
+    category: "sides"
+  },
+  {
+    name: "Mix2Pizza DIPS - Ranch",
+    image: "https://i.postimg.cc/zfJby4tL/ranch.jpg",
+    price: 0.75,
+    description: {},
+    detail: {
+      calories: 240,
+      fat: 24,
+      carbs: 4,
+      protein: 2
+    },
+    category: "sides"
+  }
+
+
 ];
 
 module.exports = itemList;
