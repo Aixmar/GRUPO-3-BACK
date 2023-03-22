73ca0d27e04742e7ddeefca13009088b97432e86
aixmar <aixamarianag@gmail.com>
2023-03-22 16:33:35 -0300

    buil(deploy): empezando

--- Api/index.js
+++ Api/index.js
@@ -1,14 +1,16 @@
+require('dotenv').config()
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const createIngredients = require('./src/update/createIngredients');
 const createPizzas = require('./src/update/createPizzas');
+const port = process.env.PORT || 3001
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   
   createIngredients();
   createPizzas();
-  server.listen(3001, () => {
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
+  server.listen(port, () => {
+    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
 });
