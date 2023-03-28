0e0fe74d87c22bff4734c6d2cce582ceb409bfe8
Jeremy23a <jeremy.andre.99@gmail.com>
2023-03-28 18:39:39 -0500

    BUILD(favorites-Hand): add favorite

--- Api/src/handlers/usersHandlers.js
+++ Api/src/handlers/usersHandlers.js
@@ -1,7 +1,19 @@
-const { getAllUsers, createUser,userLogin,refreshTokenController,logOut, updateImage, putUpdateCartController, getUserById, updateEmail, updatePassword, putUpdatePurchaseController } = require('../controllers/usersControllers');
+const {
+  getAllUsers,
+  addFavorite,
+  createUser,
+  userLogin,
+  refreshTokenController,
+  logOut,
+  updateImage,
+  putUpdateCartController,
+  getUserById,
+  updateEmail,
+  updatePassword,
+  putUpdatePurchaseController,
+} = require("../controllers/usersControllers");
 require("dotenv").config();
-const jwt = require('jsonwebtoken')
-
+const jwt = require("jsonwebtoken");
 
 const getAllUsersHandler = async (req, res) => {
   try {
@@ -12,125 +24,160 @@ const getAllUsersHandler = async (req, res) => {
   }
 };
 
-
 const postUserHandler = async (req, res) => {
+  const {
+    name,
+    lastName,
+    email,
+    password,
+    birthday,
+    image,
+    cart,
+    rol,
+    previusPurchase,
+    favorites,
+  } = req.body;
 
-    const { name, lastName, email, password, birthday, image, cart,rol,previusPurchase } = req.body;
-    
-    try {
-      const newUser = await createUser(name, lastName, email, password, birthday, image, cart,rol,previusPurchase);
-      res.status(200).json(newUser);
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    };
-  };
-  
-  const putUserHandler = async (req, res) => {
-    const {urlImage, userId } = req.body;
-    
-    try {
-      const user = await updateImage(urlImage, userId);
-      res.status(200).json(user);
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    };
-  };
-   
-  const putUpdateCart =async (req, res) => {
-    const {cart , userId} = req.body;
-    try {
-      const user = await putUpdateCartController(cart, userId)
-      res.status(200).json(user);
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
-  };
+  try {
+    const newUser = await createUser(
+      name,
+      lastName,
+      email,
+      password,
+      birthday,
+      image,
+      cart,
+      rol,
+      previusPurchase,
+      favorites,
+    );
+    res.status(200).json(newUser);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
-  const putUpdatePurchase =async (req, res) => {
-    const {cart, userId} = req.body;
-    try {
-      const user = await putUpdatePurchaseController(cart, userId)
-      res.status(200).json(user);
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
-  };
+const putUserHandler = async (req, res) => {
+  const { urlImage, userId } = req.body;
 
-  const updateEmailHandler =async (req, res) => {
-    const {email , userId} = req.body;
-    try {
-      const user = await updateEmail(email, userId);
-      res.status(200).json(user);
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
-  };
-  const updatePasswordHandler =async (req, res) => {
-    const {password , userId} = req.body;
+  try {
+    const user = await updateImage(urlImage, userId);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
-    try {
-      const user = await updatePassword(password, userId)
-  
-      res.status(200).json(user);
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
-  };
-  
+const putUpdateCart = async (req, res) => {
+  const { cart, userId } = req.body;
+  try {
+    const user = await putUpdateCartController(cart, userId);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const putUpdatePurchase = async (req, res) => {
+  const { cart, userId } = req.body;
+  try {
+    const user = await putUpdatePurchaseController(cart, userId);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const updateEmailHandler = async (req, res) => {
+  const { email, userId } = req.body;
+  try {
+    const user = await updateEmail(email, userId);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+const updatePasswordHandler = async (req, res) => {
+  const { password, userId } = req.body;
+
+  try {
+    const user = await updatePassword(password, userId);
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
 const getUserByIdHandler = async (req, res) => {
   const { id } = req.params;
   try {
-      const user = await getUserById(id);
-      res.status(200).json(user);
+    const user = await getUserById(id);
+    res.status(200).json(user);
   } catch (error) {
-      res.status(400).json({ error: error.message });
-  };
+    res.status(400).json({ error: error.message });
+  }
 };
 
-const postUserLoginHandler = async(req,res)=>{
-    const {email,password} = req.body;
-    try{
-      const { loggedInUser, refreshToken } = await userLogin(email, password);
-      res.cookie('jwt',refreshToken, {httpOnly:true,maxAge:24*60*60*1000} )
-      res.status(200).json(loggedInUser);
-
-    }catch(error){
-      res.status(400).json({ error: error.message });
-    }
-}
-
-const refreshTokenHandler = async (req,res)=>{
-  const cookies = req.cookies
-  
+const postUserLoginHandler = async (req, res) => {
+  const { email, password } = req.body;
   try {
-    const accesToken = await refreshTokenController(cookies)
-    
+    const { loggedInUser, refreshToken } = await userLogin(email, password);
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+    res.status(200).json(loggedInUser);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const refreshTokenHandler = async (req, res) => {
+  const cookies = req.cookies;
+
+  try {
+    const accesToken = await refreshTokenController(cookies);
+
     res.status(200).json(accesToken);
   } catch (error) {
-    res.status(400).json({ error: error.message }); 
+    res.status(400).json({ error: error.message });
   }
-}
+};
 
-const logOutHandler = async (req,res)=>{
-  const cookies = req.cookies
+const logOutHandler = async (req, res) => {
+  const cookies = req.cookies;
   try {
-    const response = await logOut(res,cookies)
+    const response = await logOut(res, cookies);
     res.status(200).json(response);
   } catch (error) {
-    res.status(400).json({ error: error.message }); 
+    res.status(400).json({ error: error.message });
   }
-}
+};
+
+const updateFavoritesItems = async (req, res, next) => {
+  try {
+    const { productId, userId } = req.body; // id del producto
+
+    const itemFavorite = await addFavorite(productId, userId);
+
+    res.status(200).json(itemFavorite);
+  } catch (error) {
+    res.status(404).json({error: error.message});
+  }
+};
 
 module.exports = {
-    getAllUsersHandler,
-    postUserHandler,
-    getUserByIdHandler,
-    postUserLoginHandler,
-    refreshTokenHandler,
-    logOutHandler,
-    putUserHandler,
-    putUpdateCart,
-    updateEmailHandler,
-    updatePasswordHandler,
-    putUpdatePurchase,
+  getAllUsersHandler,
+  postUserHandler,
+  getUserByIdHandler,
+  postUserLoginHandler,
+  refreshTokenHandler,
+  logOutHandler,
+  putUserHandler,
+  putUpdateCart,
+  updateEmailHandler,
+  updatePasswordHandler,
+  putUpdatePurchase,
+  updateFavoritesItems,
 };
